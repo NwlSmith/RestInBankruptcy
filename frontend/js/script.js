2914db4fb204c5b58aef23b89e0be0f1e6529081
jw5374 <jw5374@nyu.edu>
2021-12-01 19:43:40 -0500

    intial function versions for comments/flowers

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,5 @@
+const putUrl = 'http://3.144.16.89:8080/dynamoDB/doc/'
+
 function expand(arrowIndex, detailNum){
 	var arrow = document.getElementById(arrowIndex);
 	var details = document.getElementById(detailNum);
@@ -16,3 +18,32 @@ function expand(arrowIndex, detailNum){
 	}
 }
 
+function addComment(pkgId, comment) {
+	fetch(putUrl + "comments", {
+		method: "PUT",
+		headers: {
+			'Content-Type': "application/json"
+		},
+		body: JSON.stringify({
+			keyObj: {
+				packageId: pkgId
+			},
+			comment: comment
+		})
+	}).catch(e => console.log(e))
+}
+
+
+function addFlower(pkgId, flowerNum) {
+	fetch(putUrl + `flowers/${flowerNum}`, {
+		method: "PUT",
+		headers: {
+			'Content-Type': "application/json"
+		},
+		body: JSON.stringify({
+			keyObj: {
+				packageId: pkgId
+			}
+		})
+	}).catch(e => console.log(e))
+}
